83f88768c204f1d0dd58aaca734a7e9a594d9a0d
James Sansbury <james@lullabot.com>
2018-02-28 07:09:29 -0500

    Remove tokens from repo.

--- oh-my-zsh/custom/pushover.zsh
+++ oh-my-zsh/custom/pushover.zsh
@@ -1,7 +1,8 @@
+source ~/private/.pushover
 function push {
   HOSTNAME=`hostname | sed -e 's/\.local//'`
-  curl -s -F "token=aRMVpKtCozKa3bhvBedXg3L8KXVsMk" \
-    -F "user=uRHxRgrYAx6k2vyGf2qjgxbKqL7E8u" \
+  curl -s -F "token=$PUSHOVER_TOKEN" \
+    -F "user=$PUSHOVER_USER" \
     -F "title=$HOSTNAME" \
     -F "message=$1" https://api.pushover.net/1/messages.json
 }
