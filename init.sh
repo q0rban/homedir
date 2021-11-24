169d2712f19e40d6c8167ec206bdd3b5d119d70c
James Sansbury <james@lullabot.com>
2021-11-24 11:23:58 -0500

    Update home-brew install instructions

--- init.sh
+++ init.sh
@@ -63,6 +63,8 @@ install_homebrew() {
     echo "Homebrew already installed. Continuing."
   else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 }
 
