7db5d721747ba6fd69a789a219feb66d13fad578
James Sansbury <james@lullabot.com>
2022-07-25 10:47:19 -0400

    update aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,19 +1,10 @@
-# Add yourself some shortcuts to projects you often work on
-# Example:
-#
-# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
 alias ding="say 'all done' &"
-alias dru='drush use `git config --get drush.alias`'
-alias ddrush='docker-compose exec --user 0 php drush -r ./docroot $*'
-alias ddrusha='docker-compose exec --user 82 php drush $*'
-# New Vagrant Project
-alias nvp='git clone git@github.com:Lullabot/trusty32-lamp.git'
 
 # Lullabot / Tugboat servers ssh
 alias tm1="ss manager1"
 alias tm2="ss manager2"
 alias tm3="ss manager3"
+alias tm4="ss manager4"
 alias tw1="ss worker1"
 alias tw2="ss worker2"
 alias tw3="ss worker3"
@@ -28,22 +19,26 @@ alias tt="ss test"
 alias tsm1="ss manager1.stage.tugboat.qa"
 alias tsm2="ss manager2.stage.tugboat.qa"
 alias tsm3="ss manager3.stage.tugboat.qa"
-alias t1x='ss 1xinternet.tugboat.qa'
 
-alias tugboat-down='ssh -t irongoat.local "sudo shutdown now"'
-export tugboat-up() {
-  wakeonlan 54:b2:03:83:97:56
-  printf "Waiting for sshd to start."
-  while ! nc -z irongoat.local 22 &> /dev/null; do
-    sleep 1;
-    printf "."
-  done
-  printf "\nReady for connections!\n"
+tm() {
+  server=${1-1}
+  ss "manager${server}"
 }
-alias tugboat-ssh="tugboat-up && ssh irongoat.local"
+typeset -gx tm
+
+tw() {
+  server=${1-1}
+  ss "worker${server}"
+}
+typeset -gx tw
+
+tsm() {
+  server=${1-1}
+  ss "manager${server}.stage.tugboat.qa"
+}
 
 # SSH to a host and su as root.
-export ss() {
+ss() {
   if [[ -z "$1" ]]; then
     echo "You must pass the hostname to ssh with sudo to." 1>&2;
     return 23
@@ -57,6 +52,7 @@ export ss() {
     ssh -t $1 "sudo su -"
   )
 }
+typeset -gx ss
 
 prompt_ldap_password() {
   echo "[sudo] password for $(whoami):"
@@ -66,7 +62,7 @@ prompt_ldap_password() {
   stty "$stty_orig"
 }
 
-export tmdb() {
+tmdb() {
   prompt_ldap_password
 
   manager_server=$(expect -c "$(
@@ -98,5 +94,19 @@ export tmdb() {
 EOF
   )")
   echo "SSHing to $manager_server..."
-  ssh -t $manager_server 'sudo sh -c "docker exec -it \$(docker ps --filter \"name=\$(docker service ps tugboat-mongo --filter='desired-state=running' --format='{{.Name}}')\" -q) mongo"'
+  ssh -t $manager_server 'sudo sh -c "docker exec -it \$(docker ps --filter \"name=\$(docker service ps tugboat-mongo --filter='desired-state=running' --format='{{.Name}}')\" -q) mongosh"'
 }
+typeset -gx tmdb
+
+alias tugboat-down='ssh -t irongoat.local "sudo systemctl poweroff"'
+tugboat_up() {
+  wakeonlan 54:b2:03:83:97:56
+  printf "Waiting for sshd to start."
+  while ! nc -z irongoat.local 22 &> /dev/null; do
+    sleep 1;
+    printf "."
+  done
+  printf "\nReady for connections!\n"
+}
+typeset -gx tugboat_up
+alias tugboat-ssh="tugboat-up && sleep 10 && ssh irongoat.local"
